7d8d8545ba44f93f6e18a05ecebffabd860cf58f
Joonha Shin <shin.joonha@gmail.com>
2012-03-25 13:03:03 -0400

    Added doc_list, inverted_index and modified methods and run

--- method.rb
+++ method.rb
@@ -1,4 +1,7 @@
 require "zlib"
+require "stemmify"
+
+STOPWORDS = open("stopwords").read
 
 def retrieve(f,c,big_d,big_c,smoothing)
   if f.class && c.class && big_d.class && smoothing.class == Float
@@ -16,6 +19,109 @@ def read_data(filename)
   Zlib::GzipReader.open(filename).read
 end
 
+def tokenize(string)
+  terms = []
+  string.downcase.split(/[^a-z_\-<>\/0-9]/).each do |t|
+    if STOPWORDS.match(t).nil?
+      terms << t.stem
+    end
+  end
+  terms
+end
+
+def create_list(filename)
+  @doc_list = {}
+  inside_doc = false
+  in_length = false
+  docid = 0
+  record = []
+  get_data(filename).each_with_index do |line,index|
+    # if @doc_list.length > 10000
+    #   break
+    # end
+    if line.match("<DOC>")
+      inside_doc = true
+    elsif line.match("<DOCNO>") && inside_doc
+      record[0] = tokenize(line)[1]
+    elsif line.match("<DOCID>") && inside_doc
+      docid = tokenize(line)[1]
+    elsif line.match("</LENGTH>")
+      in_length = false
+    elsif line.match("<LENGTH>")
+      in_length = true
+    elsif in_length == true && line.match("<P>").nil? && line.match("</P>").nil?
+      # puts tokenize(line)[0]
+      record[1] = tokenize(line)[0].to_i
+    elsif line.match("</DOC>")
+      if record[1].nil?
+        record[1] = 1000
+      end
+      @doc_list[docid] = record
+      record = []
+      inside_doc = false
+    end
+    print "\r\e[0K#{@doc_list.length}"
+  end
+  File.open("doc_list.txt", "w") { |f|
+    @doc_list.each do |i|
+      f.write "#{i[0]},#{i[1][0]},#{i[1][1]}\n"
+    end
+  }
+end
+
+def load_index(indexname)
+  index_h = {}
+  inner_h = {}
+  key = ""
+  docid = 0
+  File.open(indexname, "r") { |file|
+    file.each_with_index do |line,i| 
+      print "\r\e[0K#{i}"
+      # if i > 20
+      #   break
+      # end
+      if i % 2 == 0
+        key = line.chop!
+      elsif i % 2 == 1
+        line.split("\t").each_with_index do |item,j|
+          # if j > 5
+          #   break
+          # end
+          if j % 2 == 0
+            # docid
+            docid = item.strip!.to_i
+          elsif j % 2 == 1
+            # tf
+            inner_h[docid] = item.strip!.to_i
+          end
+        end
+        index_h[key] = inner_h
+      end
+    end
+  }
+  index_h
+  # SANITY CHECK
+  # index_h.keys.each do |key|
+  #   puts key
+  # end
+end
+
+def load_doclist(doclistname)
+  docid = 0
+  docno = ""
+  doclength = 0
+  doc_list = {}
+  File.open(doclistname, "r") { |file|
+    file.each_with_index do |line, i|
+      items = line.split(",")
+      docid = items[0].to_i
+      docno = items[1]
+      doclength = items[2].to_i
+      doc_list[docid] = [docno, doclength]
+    end
+  }
+  doc_list
+end
 # log P(Q|D) = sum (log((1-smoothing)*(f / |D|) + (smoothing)*(c / |C|)))
 # f: number of times query word occurs in the document
 # c: number of times query word occurs in the collection
--- run.rb
+++ run.rb
@@ -1,13 +1,30 @@
 load 'method.rb'
 # all numbers should be float
-smoothing = 0.5
-f = 10.0
-c = 200.0
-big_d = 700.0
-big_c = 100000.0
-puts retrieve(f,c,big_d,big_c,smoothing)
-f += 1
-puts retrieve(f,c,big_d,big_c,smoothing)
-c += 1
-puts retrieve(f,c,big_d,big_c,smoothing)
-puts read_data("data/latimes.dat.gz")
+# smoothing = 0.5
+# f = 10.0
+# c = 200.0
+# big_d = 700.0
+# big_c = 100000.0
+# puts retrieve(f,c,big_d,big_c,smoothing)
+# f += 1
+# puts retrieve(f,c,big_d,big_c,smoothing)
+# c += 1
+# puts retrieve(f,c,big_d,big_c,smoothing)
+filename = "data/latimes.dat.gz"
+indexname = "inverted_index.txt"
+doclistname = "doc_list.txt"
+array = []
+query = "query.txt"
+t1 = Time.now
+# index = load_index(indexname)
+# doc_list = load_doclist(doclistname)
+File.open(query, "r") { |file|
+  file.each do |line|
+    array = tokenize(line)
+    array.each do |token|
+
+    end
+  end
+}
+t2 = Time.now
+puts t2 - t1
