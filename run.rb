f5602d75fd9d693e90a722b96de810eb6825cb28
Joonha Shin <shin.joonha@gmail.com>
2012-03-26 14:30:14 -0400

    Removed previous work and started working more systematically, much more comments are being added.

--- run.rb
+++ run.rb
@@ -3,76 +3,51 @@ load 'method.rb'
 filename = "data/latimes.dat.gz"
 indexname = "data/inverted_index.txt"
 doclistname = "data/doc_list.txt"
-queryname = "data/query.txt"
+queryname = "data/query_test.txt"
 
-t1 = Time.now
+# declare local variables
+topicID = 0
+query_array = []
+docID_array = []
+docno = ""
+
+t1 = Time.now # start timing process
 
 index = load_index(indexname) # loads the index
 doc_list = load_doclist(doclistname) # loads the document list
 
 puts "topicID 0 docno rank score runTag"
-File.open(queryname, "r") { |file|
+# Read list of queries (one line at at time)
+File.open(queryname, "r") do |file|
   file.each do |line|
-    entry = line.split(":")
-    query = tokenize(entry[1])
-    query.each do |q|
-      puts q
+    # Tokenize query list and store information in array
+    topicID = line.split(":")[0]
+    query_array = tokenize(line.split(":")[1])
+
+    # Collect all unique docIDs associated with the query term
+    query_array.each do |term|
+      docID_array = docID_array | index[term].keys
     end
-    puts entry[0] + " 0 "
+
+    # Iterate through each docID in docID_array
+    docID_array.each do |docID|
+      # Get docno
+      docno = doc_list[docID][0]
+      # Get |D|
+      big_d = doc_list[docID][1]
+      puts big_d
+      # Iterate through each query in query_array
+      query_array.each do |query|
+        unless index[query][docID].nil?
+          f = index[query][docID]
+        end
+      end
+      # Compute scores for each item in docID_array
+      break # docID_array
+    end
+    break # from file
   end
-}
+end
 
-# tokenize(query).each do |token|
-#   f = index[token][docid].to_f
-#   index[token].each do |k,v|
-#     sum += v
-#   end
-#   c = sum.to_f
-#   sum = 0
-#   big_d = doc_list[docid][1].to_f
-#   index.each do |k,v|
-#     v.each do |k2,v2|
-#       sum += v2
-#     end
-#   end
-#   big_c = sum.to_f
-#   sum = 0
-
-#   puts "f: #{f}"
-#   puts "c: #{c}"
-#   puts "big_d: #{big_d}"
-#   puts "big_c: #{big_c}"
-#   puts "score: #{retrieve(f,c,big_d,big_c,smoothing)}"
-# end
-
-# File.open(queryname, "r") { |file|
-#   file.each do |query|
-#     tokenize(query).each do |token|
-#       index[token].each do |k,v|
-#         sum += v
-#       end
-#       c = sum
-#       sum = 0
-#       doc_list.each do |docid,value|
-#         unless index[token][docid].nil?
-#           puts
-#           puts "docid: #{docid}"
-#           f = index[token][docid]
-#           puts "frequency: #{f}"
-#           big_d = value[1]
-#           puts "|D|: #{big_d}"
-#           puts "c: #{c}"
-#           # index[token].each do |k,v|
-#           #   sum += v
-#           # end
-#           # puts c = sum
-#           # sum = 0
-#         end
-#       end
-#       # puts "#{j}: #{index[token].length}"
-#       # puts index[token]
-#     end
-#   end
-# }
 t2 = Time.now
 puts "Total run time: #{t2 - t1} seconds"
