951264fdbdea6ee1e8671c29226d29de8b905091
Joonha Shin <shin.joonha@gmail.com>
2012-03-27 14:47:55 -0400

    Made changes to run

--- run.rb
+++ run.rb
@@ -3,51 +3,114 @@ load 'method.rb'
 filename = "data/latimes.dat.gz"
 indexname = "data/inverted_index.txt"
 doclistname = "data/doc_list.txt"
-queryname = "data/query_test.txt"
+queryname = "data/query.txt"
+resultsname = "data/results.txt"
 
 # declare local variables
 topicID = 0
 query_array = []
 docID_array = []
 docno = ""
+f = 0.0
+sum = 0.0
+c = 0.0
+big_d = 0.0
+big_c = 0.0 # this value is a constant
+total_score = 0.0
+sub_score = 0.0
+score_h = {}
+rank = 1
 
 t1 = Time.now # start timing process
 
+puts "loading the index..."
 index = load_index(indexname) # loads the index
+puts "loading doc_list..."
 doc_list = load_doclist(doclistname) # loads the document list
+
+# calculates |C|
+puts "calculating |C|"
+index.each do |key,value|
+  value.each do |k,v|
+    sum += v
+  end
+end
+big_c = sum
+sum = 0.0
 
 puts "topicID 0 docno rank score runTag"
 # Read list of queries (one line at at time)
 File.open(queryname, "r") do |file|
-  file.each do |line|
+  file.each_with_index do |line,j|
     # Tokenize query list and store information in array
     topicID = line.split(":")[0]
     query_array = tokenize(line.split(":")[1])
 
     # Collect all unique docIDs associated with the query term
-    query_array.each do |term|
-      docID_array = docID_array | index[term].keys
+    query_array.each do |query|
+      docID_array = docID_array | index[query].keys
     end
 
     # Iterate through each docID in docID_array
-    docID_array.each do |docID|
+    docID_array.each_with_index do |docID,i|
       # Get docno
       docno = doc_list[docID][0]
       # Get |D|
       big_d = doc_list[docID][1]
-      puts big_d
+
       # Iterate through each query in query_array
       query_array.each do |query|
         unless index[query][docID].nil?
           f = index[query][docID]
         end
-      end
-      # Compute scores for each item in docID_array
-      break # docID_array
-    end
-    break # from file
-  end
-end
 
+        # Compute c
+        index[query].each do |k,v|
+          sum += v
+        end
+        c = sum
+        sum = 0
+
+        # Compute sub_score for each query term
+        sub_score = retrieve(f.to_f, c.to_f, big_d.to_f, big_c.to_f, 0.5)
+
+        # Compute scores for each item in docID_array
+        total_score += sub_score
+      end # query_array
+
+      # Add total_score to a hash (to write to file later)
+      score_h[docno] = total_score
+
+      # Clear total_score for the next document
+      total_score = 0
+
+      # Gives feedback on completion process
+      print "\r\e[0K#{j+1}/#{50}: #{i+1}/#{docID_array.length}"
+    end # docID_array
+
+    # Sort hash and write to file 
+    File.open(resultsname, "w") do |f|
+      score_h.sort_by { |k,v| -1*v }.each do |k,v|
+        # Remove if rank is higher than 1000
+        if rank > 1000
+          break
+        end
+
+        # Write a line to file
+        f.write("#{topicID} 0 #{k} #{rank} #{v} j5shin\n")
+
+        # Increase rank
+        rank += 1
+      end
+    end # file: resultsname
+
+    # Reset rank
+    rank = 0
+
+    # Clear score_h to save memory
+    score_h = {}
+  end # query lines
+end # file: queryname
+puts
 t2 = Time.now
 puts "Total run time: #{t2 - t1} seconds"
